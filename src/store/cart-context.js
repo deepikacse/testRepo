94753540b7f45ccff0f640b61bda03e6f579d2b9
Deepika Das <deepika.das@sakhaglobal.com>
2023-02-21 21:25:11 +0530

    testcommit

---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Cart from '../components/Cart/Cart';
+
+const CartContext=React.createContext({
+    items:[],
+    totalAmount:0,
+    addItem: (item) =>{},
+    removeItem:(id) =>{}
+});
+export default CartContext;
